41253f959a7cc22a94a6ccde6d9d69a38c25d24e
Alexander Eklund <alexek1987@icloud.com>
2022-03-25 19:57:54 +0100

    class transform in client

--- client/collections/Todos/fetchTodos.tsx
+++ client/collections/Todos/fetchTodos.tsx
@@ -1,14 +1,19 @@
+import { TodoModel } from '@models/Todo/TodoModel'
+import { plainToClass } from 'class-transformer'
 import { db } from '../../../firebase'
 
 export const fetchTodos = (setTodos) => {
   db.collection('entries').onSnapshot((snapshot) => {
-    setTodos(
-      snapshot.docs.map((doc) => ({
-        id: doc.id,
-        mainText: doc.data().mainText,
-        secondaryText: doc.data().secondaryText,
-        status: doc.data().status,
-      })),
+    plainToClass(
+      TodoModel,
+      setTodos(
+        snapshot.docs.map((doc) => ({
+          id: doc.id,
+          mainText: doc.data().mainText,
+          secondaryText: doc.data().secondaryText,
+          status: doc.data().status,
+        })),
+      ),
     )
   })
 }
