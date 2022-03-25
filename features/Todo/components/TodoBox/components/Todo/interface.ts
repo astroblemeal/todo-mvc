1cbcc023c43664b2fcaabe9fcd53eebde8243538
Alexander Eklund <alexek1987@icloud.com>
2022-03-25 17:52:14 +0100

    update interface

--- features/Todo/components/TodoBox/components/Todo/interface.ts
+++ features/Todo/components/TodoBox/components/Todo/interface.ts
@@ -6,5 +6,5 @@ export interface TodoProps {
   status: boolean
   setIsOpenUpdateModal: React.Dispatch<React.SetStateAction<boolean>>
   isOpenUpdateModal: boolean
-  handleUpdateModal: any
+  handleUpdateModal: (string, boolean) => void
 }
